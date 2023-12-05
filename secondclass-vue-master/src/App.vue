60765a2dc43117dddc28e4d0e7ef80d4ca3d8238
afa <595611423@qq.com>
2023-12-05 21:34:56 +0800

    框架

---
+++ secondclass-vue-master/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+<!--    <router-view name="loginPage"></router-view>
+    <div>
+
+  </div>-->
+    <router-view name="content"></router-view>
+  </div>
+</template>
+
+<script>
+import SecondClassNavbar from './components/SecondClassNavbar.vue'
+import Navigate from "@/components/Navigate";
+import Page from "./components/Page"
+import Login from "./views/Login"
+
+export default {
+  data(){
+    return{
+      state: false
+    }
+  },
+  components: {
+    SecondClassNavbar,Navigate,
+    Page,Login
+  },
+
+  methods:{
+    changeState(){
+      this.state=true
+      if(localStorage.getItem("user")!==null) {
+        localStorage.removeItem("user")
+      }
+    }
+  },
+  created() {
+    if(localStorage.getItem("user")!==null){
+      this.state=false
+    }
+    localStorage.setItem("state", this.state)
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+
+</style>
+
